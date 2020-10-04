a4856bcec91c310d8243d897b1e0071b28e39c79
Jason Wiker <jason.wiker@gmail.com>
2020-10-04 09:51:06 -0600

    Initial commit

---
+++ src/KubernetesDemo/KubernetesDemoStack.cs
@@ -0,0 +1,12 @@
+using Amazon.CDK;
+
+namespace KubernetesDemo
+{
+    public class KubernetesDemoStack : Stack
+    {
+        internal KubernetesDemoStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
