d0f675e5416af95f5412a60ba682a94ba02f3b91
Speediing <jason.wiker@gmail.com>
2020-10-04 11:51:26 -0600

    Add autoscaler node group

---
+++ src/KubernetesDemo/AutoScalerNodeGroup.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using Amazon.CDK;
+using Amazon.CDK.AWS.EC2;
+using Amazon.CDK.AWS.EKS;
+using Amazon.CDK.AWS.IAM;
+
+namespace KubernetesDemo
+{
+    /// <summary>
+    /// Abstraction to work with EKS Autoscaler (e.g. one node group per availability zone)
+    /// Is autoscalled by the Kubernetes Autoscaler - <see cref="ClusterAutoscaler"/>
+    /// </summary>
+    public class AutoScalerNodeGroup : AbstractNodeGroup
+    {
+        public AutoScalerNodeGroup(Construct scope, string id, string nodeGroupName, Cluster cluster,
+            string instanceType = "c5d.large") : base(scope, $"{id}-{nodeGroupName}")
+        {
+            for (var index = 0; index < cluster.Vpc.PrivateSubnets.Length; index++)
+            {
+                var subnet = cluster.Vpc.PrivateSubnets[index];
+                CreateNodeGroup($"{id}-{nodeGroupName}", cluster, nodeGroupName, instanceType, subnet, (AZ)index, 1);
+            }
+        }
+        private static void CreateNodeGroup(string id, Cluster cluster, string nodeGroupName, string instanceType,
+            ISubnet subnet, AZ az, int actualMinSize = 1, int actualMaxSize = 5)
+        {
+            var nodegroup = cluster.AddNodegroupCapacity($"{id}-{az}", new NodegroupProps()
+            {
+                NodegroupName = $"{cluster.ClusterName}-{nodeGroupName}-{az}",
+                InstanceType = new InstanceType(instanceType),
+                MinSize = actualMinSize,
+                MaxSize = actualMaxSize,
+                Subnets = new SubnetSelection { Subnets = new[] { subnet } },
+                Tags = new Dictionary<string, string>
+                {
+                    {"k8s.io/cluster-autoscaler/enabled", ""},
+                    {"k8s.io/cluster-autoscaler/yoda", ""}
+                }
+            });
+            nodegroup.Role.AddManagedPolicy(ManagedPolicy.FromAwsManagedPolicyName("CloudWatchAgentServerPolicy"));
+            nodegroup.Role.AddToPrincipalPolicy(autoScalePolicy);
+        }
+        private static bool IsDev(Cluster cluster, DeploymentEnvironment environment)
+        {
+            return (environment == DeploymentEnvironment.Dev || environment == DeploymentEnvironment.Sandbox)
+                   && cluster.Vpc.PrivateSubnets.Length >= 2;
+        }
+
+        private enum AZ
+        {
+            a, b, c, d, e, f, g, h, i, j, k
+        }
+    }
+}
--- src/KubernetesDemo/KubernetesDemoStack.cs
+++ src/KubernetesDemo/KubernetesDemoStack.cs
@@ -17,23 +17,22 @@ namespace KubernetesDemo
             var ecr = new ContainerRegistry(this, $"{id}-container-registry", env);
             var k8 = new KubernetesCluster(this, $"{id}-cluster", Vpc, "democluster");
 
-            //if (env == DeploymentEnvironment.Dev || env == DeploymentEnvironment.Sandbox)
-            //{
+            if (env == DeploymentEnvironment.Dev || env == DeploymentEnvironment.Sandbox)
+            {
                 var sandboxCluster = new SpotFleetWorkerGroup(this, id, k8.Master);
-             
-            //}
-            //else
-            //{
-            //    // WARNING: Changing Node Groups is destructive...
-            //    // 
-            //    // Process is to add a new node group to production then delete  the old cluster group
-            //    // e.g. api-cluster-v1, api-cluster-v2, etc
-            //    // Kubernetes Compute (e.g. EKS node groups)
-            //    var apiCluster1 = new AutoScalerNodeGroup(this, id, "api-cluster-v1", k8.Master);
-            //    apiCluster1.Node.AddDependency(k8);
 
-            //    var productionDatabase = new Database(this, id, Vpc, new InstanceType("t3.medium"), Duration.Days(30));
-            //}
+            }
+            else
+            {
+                // WARNING: Changing Node Groups is destructive...
+                // 
+                // Process is to add a new node group to production then delete  the old cluster group
+                // e.g. api-cluster-v1, api-cluster-v2, etc
+                // Kubernetes Compute (e.g. EKS node groups)
+                var apiCluster1 = new AutoScalerNodeGroup(this, id, "api-cluster-v1", k8.Master);
+                apiCluster1.Node.AddDependency(k8);
+
+            }
         }
     }
 }
